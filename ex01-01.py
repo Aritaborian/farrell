1c0f9dd5b96cb46f580a343592dcd5cd83186a7a
Aritaborian <46087880+Aritaborian@users.noreply.github.com>
2019-05-18 01:20:23 +0300

    Add files via upload

---
+++ ex01-01.py
@@ -0,0 +1,15 @@
+##Page 20.
+##EXERCISE 1­1: SQUARE DANCE
+##Return to the myturtle.py program. Your first challenge is to modify the code
+##in the program using only the f o r w a r d  and r i g h t  functions so that the turtle
+##draws a square.
+
+from turtle import *
+
+fd(100)
+rt(90)
+fd(100)
+rt(90)
+fd(100)
+rt(90)
+fd(100)
