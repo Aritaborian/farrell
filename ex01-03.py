2c65c3ee2b2675e7d83c8050daaaa2f70dbd5ea9
Aritaborian <46087880+Aritaborian@users.noreply.github.com>
2019-05-18 02:06:47 +0300

    Add files via upload

---
+++ ex01-03.py
@@ -0,0 +1,14 @@
+##Page 27.
+##EXERCISE 1­3: TRI AND TRI AGAIN
+##Write a   t r i a n g l e ( )  function that will draw a triangle of a given “side length.”
+
+from turtle import *
+
+def triangle(a=100):
+    for i in range(3):
+        fd(a)
+        rt(120)
+
+triangle(50)
+triangle(120)
+triangle()
