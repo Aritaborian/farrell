554f638e859d6430f31291ae05d9f89289834fb8
Aritaborian <46087880+Aritaborian@users.noreply.github.com>
2019-05-18 01:28:09 +0300

    Add files via upload

---
+++ ex01-02.py
@@ -0,0 +1,18 @@
+##Page 25.
+##EXERCISE 1­2: A CIRCLE OF SQUARES
+##Write and run a function that draws 60 squares, turning right 5 degrees after
+##each square. Use a loop! Your result should end up looking like this...
+
+
+from turtle import *
+
+speed(.1)
+
+def square():
+    for i in range(4):
+        fd(100)
+        rt(90)
+
+for i in range(60):
+    square()
+    rt(5)
