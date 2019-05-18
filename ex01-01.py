d471f3f498fd3cdcb44bdbce8d2011b037006324
Aritaborian <46087880+Aritaborian@users.noreply.github.com>
2019-05-18 14:30:05 +0300

    Update ex01-01.py

--- ex01-01.py
+++ ex01-01.py
@@ -1,5 +1,5 @@
 ##Page 20.
-##EXERCISE 1­1: SQUARE DANCE
+##EXERCISE 1-1: SQUARE DANCE
 ##Return to the myturtle.py program. Your first challenge is to modify the code
 ##in the program using only the f o r w a r d  and r i g h t  functions so that the turtle
 ##draws a square.
