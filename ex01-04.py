a815a5076c155db44e4c7a4da374a70cdcfde032
Aritaborian <46087880+Aritaborian@users.noreply.github.com>
2019-05-18 03:18:05 +0300

    Add files via upload

---
+++ ex01-04.py
@@ -0,0 +1,16 @@
+##Page 29.
+##EXERCISE 1­4: POLYGON FUNCTIONS
+##Write a function called p o l y g o n  that takes an integer as an argument and
+##makes the turtle draw a polygon with that integer’s number of sides.
+
+from turtle import *
+
+def ngon(n=3, a=100):
+    for i in range(n):
+        fd(a)
+        rt(360 / n)
+
+ngon()
+ngon(5)
+ngon(4, 100)
+ngon(12, 50)
