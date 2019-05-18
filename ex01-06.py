5aecff3fc1f44bebe642ebb5199607248ee1385e
Aritaborian <46087880+Aritaborian@users.noreply.github.com>
2019-05-18 05:12:45 +0300

    Update ex01-06.py

--- ex01-06.py
+++ ex01-06.py
@@ -1,3 +1,10 @@
+##Page 33.
+##EXERCISE 1­6: A STAR IS BORN
+##First, write a “star” function that will draw a five­pointed star, like this...
+##Next, write a function called s t a r S p i r a l ( )  that will draw a spiral of stars, like
+##this...
+
+
 from turtle import *
 
 speed(.1)
@@ -7,12 +14,13 @@ def star(n=5, a=100):
         fd(a)
         rt(180 - 180 / n)
 
-
-a = 50
-for i in range(60):
-    star(5, a)
-    rt(5)
-    a += 5
-
+def starSpiral():
+    a = 50
+    for i in range(60):
+        star(5, a)
+        rt(5)
+        a += 5
+        
+starSpiral()
 
 print("Done.")
